6d6f91add787da8ea92d118cd1c4613f8ad77cc1
Hajar Fatheddine <fatheddinehajar@gmail.com>
2022-04-11 05:38:54 +0000

    Adding an alert box to code quiz

--- app/src/main/java/com/example/application_quiz/MainActivity3.java
+++ app/src/main/java/com/example/application_quiz/MainActivity3.java
@@ -1,11 +1,15 @@
 package com.example.application_quiz;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -13,6 +17,7 @@ public class MainActivity3 extends AppCompatActivity {
     CardView quiz2;
     CardView quiz3;
     CardView quiz4;
+    FirebaseAuth mauth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,4 +54,76 @@ public class MainActivity3 extends AppCompatActivity {
         });
 
     }
+    @Override
+    public void onBackPressed()
+    {
+
+        // Create the object of
+        // AlertDialog Builder class
+        AlertDialog.Builder builder
+                = new AlertDialog
+                .Builder(MainActivity3.this);
+
+        // Set the message show for the Alert time
+        builder.setMessage("are you sure you want to logout");
+
+        // Set Alert Title
+        builder.setTitle("LOGOUT");
+
+        // Set Cancelable false
+        // for when the user clicks on the outside
+        // the Dialog Box then it will remain show
+        builder.setCancelable(false);
+
+        // Set the positive button with yes name
+        // OnClickListener method is use of
+        // DialogInterface interface.
+
+        builder
+                .setPositiveButton(
+                        "Yes",
+                        new DialogInterface
+                                .OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which)
+                            {
+
+                                // When the user click yes button
+                                // then app will close
+                                Intent intent = new Intent(MainActivity3.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+
+                            }
+                        });
+
+        // Set the Negative button with No name
+        // OnClickListener method is use
+        // of DialogInterface interface.
+        builder
+                .setNegativeButton(
+                        "No",
+                        new DialogInterface
+                                .OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which)
+                            {
+
+                                // If user click no
+                                // then dialog box is canceled.
+                                dialog.cancel();
+                            }
+                        });
+
+        // Create the Alert dialog
+        AlertDialog alertDialog = builder.create();
+
+        // Show the Alert Dialog box
+        alertDialog.show();
+    }
+
 }
